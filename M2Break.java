9f73afd2c10f4a250a0fe2d94e9936a3b87ed8ec
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:50:56 +0530

    Update M2Break.java

--- M2Break.java
+++ M2Break.java
@@ -1,1 +1,9 @@
+public class M2Break {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            if(i==5) break;
+            System.out.println(i);
+        }
+    }
+}
 
