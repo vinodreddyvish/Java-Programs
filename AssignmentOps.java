df1a8d444d015456db5fefb1be0b583069cbac3a
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 20:06:03 +0530

    Create AssignmentOps.java

---
+++ AssignmentOps.java
@@ -0,0 +1,14 @@
+public class AssignmentOps {
+    public static void main(String[] args) {
+        int x = 10;
+
+        x += 5;
+        System.out.println("After += : " + x);
+
+        x -= 3;
+        System.out.println("After -= : " + x);
+
+        x *= 2;
+        System.out.println("After *= : " + x);
+    }
+}
