6e2ea4a486c24a8337afadc77a95b4c3e891bb53
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:42:05 +0530

    Create NumberCheck.java

---
+++ NumberCheck.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if(n > 0)
+            System.out.println("Positive");
+        else if(n < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
+}
