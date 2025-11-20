87ec11f507a9817ceb10676b34497a69882cd8f8
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:29:43 +0530

    Add details class to capture user name and age

---
+++ details.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class details {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name: ");
+        String Name= sc.nextLine();
+        System.out.println("Enter your Age: ");
+        int Age= sc.nextInt();
+        System.out.println("I am "+ Name + ",I am "+ Age + "Years Old!!");
+
+    }
+}
