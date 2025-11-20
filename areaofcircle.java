242860099e5bae972f8be5d3e8258261d94e0ac8
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:34:35 +0530

    Create areaofcircle.java

---
+++ areaofcircle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class areaofcircle {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the Radius of Circle: ");
+        float r= sc.nextFloat();
+        System.out.println("Area of Circle is: "+ 3.142*(r*r));
+    }
+}
