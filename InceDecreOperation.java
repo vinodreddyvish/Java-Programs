2d3c92d20e6eaa1d65e2a864197d9798d4ba3ae5
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 20:03:37 +0530

    Create InceDecreOperation.java

---
+++ InceDecreOperation.java
@@ -0,0 +1,11 @@
+public class InceDecreOperation {
+    public static void main(String[] args) {
+        int a = 5;
+
+        System.out.println("a = " + a);
+        System.out.println("a++ = " + a++);
+        System.out.println("++a = " + ++a);
+        System.out.println("a-- = " + a--);
+        System.out.println("--a = " + --a);
+    }
+}
