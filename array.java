5fbc9d571d30ec89ba72282d63f5ccdbae45cbd8
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:34:03 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,12 @@
+class array {
+public static void main(String[] args){
+    int n[] = new int[5];
+    n[0]=1;
+    n[1]=2;
+    n[2]=3;
+    n[3]=4;
+    n[4]=5;
+    System.out.println(n[4]);
+
+}    
+}
