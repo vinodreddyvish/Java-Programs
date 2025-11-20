f7202c1ec5fa026a50ccc20e4abb23a395a34006
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:27:06 +0530

    Add Java program to check if a number is positive

---
+++ learn.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class learn{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int a = sc.nextInt();
+        if(a>0){
+            System.out.println("The Number is Positive!!");
+
+        }
+        else{
+            System.out.println("The Number is Negative!!");
+        }
+    }
+}
