0beab36ce933752e34cf93af5d34083a1292291f
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:21:00 +0530

    Add str class to demonstrate string case conversion

---
+++ str.java
@@ -0,0 +1,8 @@
+public class str {
+    public static void main (String[] args){
+        String a = "Hello";
+        System.out.println(a);
+        System.out.println("Upper Case is Written as: "+ a.toUpperCase());
+        System.out.println("Lower Case is Written as "+ a.toLowerCase());
+    }
+}
