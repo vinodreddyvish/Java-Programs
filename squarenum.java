4c3a413f7a14bd96994aa766e06ee9e20488874c
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:22:27 +0530

    Add Java program to calculate square of a number

---
+++ squarenum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class squarenum {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number: ");
+        int a = sc.nextInt();
+        int d=a*a;
+        System.out.println("Square of "+a +" is "+d);
+
+    }
+    
+}
