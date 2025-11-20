38ecabcd1b49d6bc0794ae7e9223ae640221e432
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:33:20 +0530

    Add arrayofeven.java to display even numbers
    Prints even numbers from a predefined array.

---
+++ arrayofeven.java
@@ -0,0 +1,11 @@
+public class arrayofeven {
+    public static void main(String[] args){
+        int [] arr={1,2,3,4,5,6,7,8,10};
+        System.out.println("Even Numbers From the Array: ");
+        for(int i=0;i<arr.length;i++){
+            if (arr[i]%2==0){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
