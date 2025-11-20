5c70a0359b587dd7310e31bee1d81c553795e0a6
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:51:57 +0530

    Implement voting eligibility check based on age and citizenship

--- LogicalOps.java
+++ LogicalOps.java
@@ -1,1 +1,18 @@
+import java.util.*;
+
+public class LogicalOps {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+        System.out.print("Are you a citizen? (true/false): ");
+        boolean citizen = sc.nextBoolean();
+
+        if(age >= 18 && citizen)
+            System.out.println("Eligible to vote");
+        else
+            System.out.println("Not eligible to vote");
+    }
+}
 
