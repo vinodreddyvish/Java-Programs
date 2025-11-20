8487653056142783ef4003a25058b684a53e5cdb
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:46:50 +0530

    Add M2IfElse class to check positive or negative

---
+++ M2IfElse.java
@@ -0,0 +1,7 @@
+public class M2IfElse {
+    public static void main(String[] args) {
+        int n = 10;
+        if(n > 0) System.out.println("Positive");
+        else System.out.println("Negative");
+    }
+}
