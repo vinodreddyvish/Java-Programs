5acf81a680c8aad3bf2b963e6838ccbf4c35c76e
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:45:36 +0530

    Add M2IfNested class to check positive even numbers

---
+++ M2IfNested.java
@@ -0,0 +1,8 @@
+public class M2IfNested {
+    public static void main(String[] args) {
+        int n = 50;
+        if(n > 0){
+            if(n % 2 == 0) System.out.println("Positive Even");
+        }
+    }
+}
