60aef7cc6e9281b5c0133eb29cea5be2e1ca1c79
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:26:31 +0530

    Implement max element finder in an array

---
+++ maxelemarray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class maxelemarray {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number of element in the Array: ");
+        int a = sc.nextInt();
+        int check=0;
+        int[] arr= new int[a];
+        for(int i=0;i<a;i++){
+            System.out.println("Enter the Number of Elements in the Array: ");
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<a;i++){
+            if(arr[i]>check){
+                check=arr[i];
+            }
+        }
+        System.out.println("Greatest Number in the Array is: "+check);
+
+}
+}
