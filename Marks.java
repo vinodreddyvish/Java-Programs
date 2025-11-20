9fb50951f3461590247ab89810c6aab325c68dd7
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:44:35 +0530

    Create Marks.java

---
+++ Marks.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Marks {
+    public static void main(String[] args) {
+        int marks = new Scanner(System.in).nextInt();
+        System.out.println("Marks: " + marks);
+    }
+}
