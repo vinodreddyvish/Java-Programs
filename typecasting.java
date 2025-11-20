ebe907aeb38c1bbf58e1829535ac2ee70030765a
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 09:11:59 +0530

    Add typecasting example in typecasting.java

---
+++ typecasting.java
@@ -0,0 +1,9 @@
+public class typecasting {
+    public static void main(String[] args) {
+        double d = 9.8;
+        int i = (int) d;
+
+        System.out.println("Double: " + d);
+        System.out.println("Integer: " + i);
+    }
+}
