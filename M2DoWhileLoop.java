962f442005b786291ceeb657b0bfc72e9ea60374
vinod reddy <vinodreddy7629@gmail.com>
2025-11-20 19:48:40 +0530

    Add M2DoWhileLoop class with do-while example

---
+++ M2DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class M2DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while(i <= 5);
+    }
+}
